dfaf62649c8f16ecfc6f1da950a739bd5ee1817d
apastors <a.pastor.sanchez@gmail.com>
2017-12-24 00:15:53 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'readers',
   packages = ['readers'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.2',
   description = 'Easy reading',
   author = 'apastors',
   author_email = 'a.pastor.sanchez@gmail.com',
   url = 'https://github.com/apastors/readers', # use the URL to the github repo
-  download_url = 'https://github.com/apastors/readers/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/apastors/readers/archive/0.1.2.tar.gz', # I'll explain this in a second
   keywords = ['reading', 'json', 'csv'], # arbitrary keywords
   classifiers = [],
 )
