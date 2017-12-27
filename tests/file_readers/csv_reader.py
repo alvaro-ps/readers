92fff5b0b9a0acb53647af2f915431a4d93561fa
apastors <a.pastor.sanchez@gmail.com>
2017-12-28 00:03:42 +0100

    reorganize tests

---
+++ tests/file_readers/csv_reader.py
@@ -0,0 +1,24 @@
+import os
+
+from readers.file_readers import CSVReader
+
+csv_file = os.sep + os.path.relpath('../tests/files/test.csv', '/')
+class TestCSVReader(object):
+    def test_whole_file(self):
+        expected = [
+            {"a": "1", "b": "2", "c": "3", "d": "4"},
+            {"a": "5", "b": "6", "c": "7", "d": "8"},
+            {"a": "9", "b": "10", "c": "11", "d": "12"},
+        ]
+        with CSVReader(csv_file) as csv_record:
+            assert csv_record == expected
+
+    def test_iter_file(self):
+        expected = [
+            {"a": "1", "b": "2", "c": "3", "d": "4"},
+            {"a": "5", "b": "6", "c": "7", "d": "8"},
+            {"a": "9", "b": "10", "c": "11", "d": "12"},
+        ]
+        with CSVReader(csv_file, iterable=True, header=True) as reader:
+            for csv_record, expected_record in zip(reader, expected):
+                assert csv_record == expected_record
---
+++ tests/file_readers/test_readers.py
@@ -0,0 +1,46 @@
+import os
+
+import pytest
+
+from readers import FileReader
+
+textfile = os.sep + os.path.relpath('../tests/files/test1.txt', '/')
+class TestFileReader(object):
+    def test_init(self):
+        f = FileReader(textfile)
+        assert f.filename == textfile
+        assert not f.iterable
+        assert f.encoding == "utf-8"
+        f.close()
+
+    def test_enter(self):
+        f = FileReader(textfile)
+        result = f.__enter__()
+        expected = "Hola, esta\nes una cadena\nde texto"
+        assert result == expected
+        f.close()
+
+        f = FileReader(textfile, iterable=True)
+        result = f.__enter__()
+        expected = f
+        assert result == expected
+        f.close()
+
+    def test_whole_fjile(self):
+        expected = "Hola, esta\nes una cadena\nde texto"
+        with FileReader(textfile) as filetext:
+            assert filetext == expected
+
+    def test_iter_file(self):
+        expected = ["Hola, esta", "es una cadena", "de texto"]
+        with FileReader(textfile, iterable=True) as reader:
+            assert reader.encoding == "utf-8"
+            assert reader.filename == textfile
+            assert reader.iterable == True
+            for line, expected_line in zip(reader, expected):
+                assert line == expected_line
+
+    def test_raises_exception(self):
+        with pytest.raises(ValueError):
+            with FileReader(textfile) as filetext:
+                raise ValueError
