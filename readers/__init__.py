4daabef0afefabb17510b2e83871ae427f9ea467
apastors <a.pastor.sanchez@gmail.com>
2018-01-14 19:44:58 +0100

    version 1.0

--- readers/__init__.py
+++ readers/__init__.py
@@ -2,4 +2,4 @@ from .file_readers.file_reader import FileReader
 from .file_readers.json_reader import JSONReader
 from .file_readers.csv_reader import CSVReader
 
-__version__ = "0.1.3"
+__version__ = "1.0"
