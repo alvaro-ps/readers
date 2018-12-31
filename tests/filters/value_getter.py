14906df781f19da9335ddf62ceaf1ab258ed7b23
apastors <alvaro@predicube.com>
2018-12-31 15:03:20 +0100

    update value_Getter tests;

--- tests/filters/value_getter.py
+++ tests/filters/value_getter.py
@@ -3,74 +3,87 @@ from pytest import raises
 from readers.filters.value_getters import ValueGetter
 from readers.filters.operations import identity
 
-def test_compile_passes():
-    getter_str = '.key[]'   
+class TestValueGetter():
+    def test__init__ok_notransfrom(self):
+        getter_str = '.key[]'
 
-    getter = ValueGetter(getter_str)
-    assert isinstance(getter, ValueGetter)
-    assert getter.transform is identity
+        getter = ValueGetter(getter_str)
+        assert isinstance(getter, ValueGetter)
+        assert getter.transform is identity
 
-    transform = 'len'
-    getter = ValueGetter(getter_str, transform=transform)
-    assert isinstance(getter, ValueGetter)
-    assert getter.transform is not identity
+    def test__init__ok_notransfrom(self):
+        getter_str = '.key[]'
+        transform = 'len'
 
-def test_compile_fails():
-    getter_str = 'wrong jq query'
+        getter = ValueGetter(getter_str, transform=transform)
+        assert isinstance(getter, ValueGetter)
+        assert getter.transform is not identity
 
-    with raises(ValueError) as err:
+    def test__init__fails(self):
+        getter_str = 'wrong jq query'
+
+        with raises(ValueError) as err:
+            getter = ValueGetter(getter_str)
+
+    def test__str__no_transform(self):
+        getter_str = '.key'
+        expected = '.key'
+
+        getter = ValueGetter(getter_str)
+        result = str(getter)
+        assert result == expected
+
+    def test__str__transform(self):
+        getter_str = '.key'
+        transform = 'len'
+        expected = '.key (transform: len)'
+
+        getter = ValueGetter(getter_str, transform=transform)
+        result = str(getter)
+        assert result == expected
+
+    def test__repr__no_transform(self):
+        getter_str = '.key'
+        expected = '.key'
+
+        getter = ValueGetter(getter_str)
+        result = repr(getter)
+        assert result == expected
+
+    def test__repr__transform(self):
+        getter_str = '.key'
+        transform = 'len'
+        expected = '.key (transform: len)'
+
+        getter = ValueGetter(getter_str, transform=transform)
+        result = repr(getter)
+        assert result == expected
+
+    def test__call__ok_return_value(self):
+        d = {'key': 1}
+        getter_str = '.key'
+        expected = 1
+
         getter = ValueGetter(getter_str)
 
-def test_call_return_value():
-    d = {'key': 1}
-    getter_str = '.key'
-    expected = 1
+        result = getter(d)
+        assert result == expected
 
-    getter = ValueGetter('.key')
+    def test__call__ok_return_list(self):
+        d = {'key': [1, 2, 3]}
+        getter_str = '.key'
+        expected = [1, 2, 3]
 
-    result = getter(d)
-    assert result == expected
+        getter = ValueGetter(getter_str)
 
-def test_call_return_list():
-    d = {'key': [1, 2, 3]}
-    getter_str = '.key'
-    expected = [1, 2, 3]
+        result = getter(d)
+        assert result == expected
 
-    getter = ValueGetter('.key')
+    def test__call__fails(self):
+        d = {'key': [1, 2, 3]}
+        getter_str = '.wrong_key'
 
-    result = getter(d)
-    assert result == expected
+        getter = ValueGetter(getter_str)
 
-def test__str__no_transform():
-    getter_str = '.key'
-    expected = '.key'
-
-    getter = ValueGetter(getter_str)
-    result = str(getter)
-    assert result == expected
-
-def test__str__transform():
-    getter_str = '.key'
-    transform = 'len'
-    expected = '.key (transform: len)'
-
-    getter = ValueGetter(getter_str, transform=transform)
-    result = str(getter)
-    assert result == expected
-
-def test__repr__no_transform():
-    getter_str = '.key'
-    expected = '.key'
-
-    getter = ValueGetter(getter_str)
-    result = repr(getter)
-    assert result == expected
-
-def test__repr__transform():
-    getter_str = '.key'
-    transform = 'len'
-    expected = '.key (transform: len)'
-
-    getter = ValueGetter(getter_str, transform=transform)
-    result = repr(getter)
-    assert result == expected
+        result = getter(d)
+        assert result == None
