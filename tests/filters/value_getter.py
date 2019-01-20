7012e9e26eb22a3a1c52f4ef6754a34ad50ad9ac
apastors <alvaro@predicube.com>
2019-01-20 22:29:30 +0100

    modify value_getters

--- tests/filters/value_getter.py
+++ tests/filters/value_getter.py
@@ -4,26 +4,20 @@ from readers.filters.value_getters import ValueGetter
 from readers.filters.operations import identity
 
 class TestValueGetter():
-    def test__init__ok_notransfrom(self):
+    def test__init__notransfrom(self):
         getter_str = '.key[]'
 
         getter = ValueGetter(getter_str)
         assert isinstance(getter, ValueGetter)
         assert getter.transform is identity
 
-    def test__init__ok_notransfrom(self):
+    def test__init__transform(self):
         getter_str = '.key[]'
         transform = 'len'
 
         getter = ValueGetter(getter_str, transform=transform)
         assert isinstance(getter, ValueGetter)
         assert getter.transform is not identity
-
-    def test__init__fails(self):
-        getter_str = 'wrong jq query'
-
-        with raises(ValueError) as err:
-            getter = ValueGetter(getter_str)
 
     def test__str__no_transform(self):
         getter_str = '.key'
@@ -75,6 +69,16 @@ class TestValueGetter():
         expected = [1, 2, 3]
 
         getter = ValueGetter(getter_str)
+
+        result = getter(d)
+        assert result == expected
+
+    def test__call__ok_return_constant_value(self):
+        d = {}
+        value = [1, 2, 3]
+        expected = [1, 2, 3]
+
+        getter = ValueGetter(value)
 
         result = getter(d)
         assert result == expected
