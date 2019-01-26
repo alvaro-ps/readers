27a1065d93b8715ba05a3ade04b622be31b3dc2a
apastors <alvaro@predicube.com>
2019-01-26 18:46:59 +0100

    update extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.recommonmark'
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
